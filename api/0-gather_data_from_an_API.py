43b350cefc16831b3b8113210e3530a0505a7990
LNCarrion <lestycarrion@gmail.com>
2024-04-15 19:23:55 -0400

    file 0 and 1

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""collecting data from API"""
+import sys
+import requests
+
+def todo_list(employee_id):
+    """
+    this function will fetch the URL, user info, 
+    TODO list and display the employee progress
+    """
+    base_url = 'https://jsonplaceholder.typicode.com'
+
+    # User info
+    user_response = requests.get(f'{base_url}/users/{employee_id}')
+    user_data = user_response.json()
+    employee_name = user_data['name']
+    
+    # getting TODO list
+    todos_response = requests.get(f'{base_url}/todos?userId={employee_id}')
+    todos_data = todos_response.json()
+
+    # task progress
+    total_task = len(todos_data)
+    done_tasks = sum(1 for todo in todos_data if todo['completed'])
+
+    # display progress
+    print(f'Employee {employee_name} is done with tasks({done_tasks}/{total_task}):')
+    for todo in todos_data:
+        if todo['completed']:
+            print(f'\t{todo["title"]}')
+
+if __name__ == "__main__":
+    if len(sys.argv) !=2:
+        print("usage: python script.py <employee_id>")
+        sys.exit(1)
+
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print("Employee ID must be an integer")
+        sys.exit(1)
+
+    todo_list(employee_id)
---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""collecting data from API"""
+import csv
+import sys
+import requests
+
+def todo_list(employee_id):
+    """
+    this function will fetch the URL, user info, 
+    TODO list and display the employee progress
+    """
+    base_url = 'https://jsonplaceholder.typicode.com'
+
+    # User info
+    user_response = requests.get(f'{base_url}/users/{employee_id}')
+    user_data = user_response.json()
+    user_id = user_data['id']
+    username = user_data['username']
+    
+    # getting TODO list
+    todos_response = requests.get(f'{base_url}/todos?userId={employee_id}')
+    todos_data = todos_response.json()
+
+    # task progress
+    total_task = len(todos_data)
+    done_tasks = sum(1 for todo in todos_data if todo['completed'])
+
+    # display progress
+    print(f'Employee {username} is done with tasks({done_tasks}/{total_task}):')
+    for todo in todos_data:
+        if todo['completed']:
+            print(f'\t{todo["title"]}')
+
+    #export data to CSV
+    filename = f'{user_id}.csv'
+    with open(filename, 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE'])
+        for todo in todos_data:
+            csvwriter.writerow([user_id, username, todo['completed'], todo['title']])
+
+    print(f'Data exported to {filename}')
+if __name__ == "__main__":
+    if len(sys.argv) !=2:
+        print("usage: python script.py <employee_id>")
+        sys.exit(1)
+
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print("Employee ID must be an integer")
+        sys.exit(1)
+
+    todo_list(employee_id)
