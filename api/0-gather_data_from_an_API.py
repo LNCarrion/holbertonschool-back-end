90185fd0aab12fad39d66b992502664e6462f04b
LNCarrion <lestycarrion@gmail.com>
2024-04-16 10:43:42 -0400

    fix doc

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -12,20 +12,18 @@ def todo_list(employee_id):
 
     base_url = 'https://jsonplaceholder.typicode.com'
 
-    """ User info """
+
     user_response = requests.get(f'{base_url}/users/{employee_id}')
     user_data = user_response.json()
     employee_name = user_data['name']
 
-    """ getting TODO list """
     todos_response = requests.get(f'{base_url}/todos?userId={employee_id}')
     todos_data = todos_response.json()
 
-    """task progress"""
     total_task = len(todos_data)
     done_tasks = sum(1 for todo in todos_data if todo['completed'])
 
-    """ display progress """
+
     print(f'Employee {employee_name} /
           is done with tasks({done_tasks} / {total_task}): ')
     for todo in todos_data:
